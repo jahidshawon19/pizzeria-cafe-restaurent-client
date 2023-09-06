322175d03bbffe66e9d21acc523323fe445d36bc
jahidshawon19 <jahidshawon1730@gmail.com>
2023-09-06 00:15:20 -0700

    redesign order card and fetch menu and reviews data

--- src/Component/Order/Order.jsx
+++ src/Component/Order/Order.jsx
@@ -10,7 +10,7 @@ const Order = () => {
             <Helmet>
                 <title>Pizzeria Cafe | Order </title>
             </Helmet>
-            <SectionBanner title={"Order"} img={"https://images.pexels.com/photos/4033404/pexels-photo-4033404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}></SectionBanner>
+            <SectionBanner title={"Order Items"} img={"https://images.pexels.com/photos/4033404/pexels-photo-4033404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}></SectionBanner>
 
             <section className="order-section py-5">
                 <div className="container">
